860a89dd3c7b40ce9b20d3cb390f57e93d273baa
Mitko Tschimev <mitko.tschimev@icloud.com>
2016-05-10 13:33:45 +0200

    Fix for IE 'wrongDocumentError'
    Fix for browser (IE, maybe other too) which are throwing 'WrongDocumentError' f you replace an element which is not in the document

--- svg-injector.js
+++ svg-injector.js
@@ -373,6 +373,11 @@
         styleTag.textContent += '';
       });
 
+      // Fix for browser (IE, maybe other too) which are throwing 'WrongDocumentError'
+      // if you replace an element which is not in the document
+      if (document.importNode) {
+        svg = document.importNode('svg', true);
+      }
       // Replace the image with the svg
       el.parentNode.replaceChild(svg, el);
 
