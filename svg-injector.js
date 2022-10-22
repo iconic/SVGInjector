40254792bceb45c7bdc8cf5810634d803e4ee4b8
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2022-10-22 17:31:18 +0200

    v1.1.8
    - addressed security issues
    - replaced uglifyjs w/ babel & presets
    - adjusted the `build` script
    - improved `svg-injector.js`
    - bumped to v1.1.8

--- svg-injector.js
+++ svg-injector.js
@@ -6,20 +6,17 @@
  * @license MIT
  */
 
-(function (window, document) {
-
-  'use strict';
-
+((window, {implementation}) => {
   // Environment
-  var isLocal = window.location.protocol === 'file:';
-  var hasSvgSupport = document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#BasicStructure', '1.1');
+  const isLocal = window.location.protocol == 'file:';
+  const hasSvgSupport = implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#BasicStructure', '1.1');
 
   function uniqueClasses(list) {
     list = list.split(' ');
 
-    var hash = {};
-    var i = list.length;
-    var out = [];
+    const hash = {};
+    let i = list.length;
+    const out = [];
 
     while (i--) {
       if (!hash.hasOwnProperty(list[i])) {
@@ -35,13 +32,15 @@
    * cache (or polyfill for <= IE8) Array.forEach()
    * source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
    */
-  var forEach = Array.prototype.forEach || function (fn, scope) {
-    if (this === void 0 || this === null || typeof fn !== 'function') {
+  const forEach = Array.prototype.forEach || function (fn, scope) {
+    if (this == void 0 || this == null || typeof fn !== 'function') {
       throw new TypeError();
     }
 
     /* jshint bitwise: false */
-    var i, len = this.length >>> 0;
+    let i;
+
+    const len = this.length >>> 0;
     /* jshint bitwise: true */
 
     for (i = 0; i < len; ++i) {
@@ -52,32 +51,30 @@
   };
 
   // SVG Cache
-  var svgCache = {};
+  const svgCache = {};
 
-  var injectCount = 0;
-  var injectedElements = [];
+  let injectCount = 0;
+  const injectedElements = [];
 
   // Request Queue
-  var requestQueue = [];
+  const requestQueue = [];
 
   // Script running status
-  var ranScripts = {};
+  const ranScripts = {};
 
-  var cloneSvg = function (sourceSvg) {
-    return sourceSvg.cloneNode(true);
-  };
+  const cloneSvg = sourceSvg => sourceSvg.cloneNode(true);
 
-  var queueRequest = function (url, callback) {
+  const queueRequest = (url, callback) => {
     requestQueue[url] = requestQueue[url] || [];
     requestQueue[url].push(callback);
   };
 
-  var processRequestQueue = function (url) {
-    for (var i = 0, len = requestQueue[url].length; i < len; i++) {
+  const processRequestQueue = url => {
+    for (let i = 0, len = requestQueue[url].length; i < len; i++) {
       // Make these calls async so we avoid blocking the page/renderer
       /* jshint loopfunc: true */
-      (function (index) {
-        setTimeout(function () {
+      (index => {
+        setTimeout(() => {
           requestQueue[url][index](cloneSvg(svgCache[url]));
         }, 0);
       })(i);
@@ -85,7 +82,9 @@
     }
   };
 
-  var loadSvg = function (url, callback) {
+  const loadSvg = (url, callback) => {
+    if (!window.SVGSVGElement) return;
+
     if (svgCache[url] !== undefined) {
       if (svgCache[url] instanceof SVGSVGElement) {
         // We already have it in cache, so use it
@@ -107,15 +106,15 @@
       svgCache[url] = {};
       queueRequest(url, callback);
 
-      var httpRequest = new XMLHttpRequest();
+      const httpRequest = new XMLHttpRequest();
 
-      httpRequest.onreadystatechange = function () {
+      httpRequest.onreadystatechange = () => {
         // readyState 4 = complete
-        if (httpRequest.readyState === 4) {
+        if (httpRequest.readyState == 4) {
 
           // Handle status
-          if (httpRequest.status === 404 || httpRequest.responseXML === null) {
-            callback('Unable to load SVG file: ' + url);
+          if (httpRequest.status == 404 || httpRequest.responseXML == null) {
+            callback(`Unable to load SVG file: ${url}`);
 
             if (isLocal) callback('Note: SVG injection ajax calls do not work locally without adjusting security setting in your browser. Or consider using a local webserver.');
 
@@ -124,7 +123,7 @@
           }
 
           // 200 success from server, or 0 when using file:// protocol locally
-          if (httpRequest.status === 200 || (isLocal && httpRequest.status === 0)) {
+          if (httpRequest.status == 200 || (isLocal && httpRequest.status == 0)) {
 
             /* globals Document */
             if (httpRequest.responseXML instanceof Document) {
@@ -141,9 +140,9 @@
             //
             // :NOTE: IE8 and older doesn't have DOMParser, but they can't do SVG either, so...
             else if (DOMParser && (DOMParser instanceof Function)) {
-              var xmlDoc;
+              let xmlDoc;
               try {
-                var parser = new DOMParser();
+                const parser = new DOMParser();
                 xmlDoc = parser.parseFromString(httpRequest.responseText, 'text/xml');
               }
               catch (e) {
@@ -151,7 +150,7 @@
               }
 
               if (!xmlDoc || xmlDoc.getElementsByTagName('parsererror').length) {
-                callback('Unable to parse SVG file: ' + url);
+                callback(`Unable to parse SVG file: ${url}`);
                 return false;
               }
               else {
@@ -164,7 +163,7 @@
             processRequestQueue(url);
           }
           else {
-            callback('There was a problem injecting the SVG: ' + httpRequest.status + ' ' + httpRequest.statusText);
+            callback(`There was a problem injecting the SVG: ${httpRequest.status} ${httpRequest.statusText}`);
             return false;
           }
         }
@@ -181,14 +180,14 @@
   };
 
   // Inject a single element
-  var injectElement = function (el, evalScripts, pngFallback, callback) {
+  const injectElement = (el, evalScripts, pngFallback, callback) => {
 
     // Grab the src or data-src attribute
-    var imgUrl = el.getAttribute('data-src') || el.getAttribute('src');
+    const imgUrl = el.getAttribute('data-src') || el.getAttribute('src');
 
     // We can only inject SVG
     if (!(/\.svg/i).test(imgUrl)) {
-      callback('Attempted to inject a file with a non-svg extension: ' + imgUrl);
+      callback(`Attempted to inject a file with a non-svg extension: ${imgUrl}`);
       return;
     }
 
@@ -196,7 +195,7 @@
     // either defined per-element via data-fallback or data-png,
     // or globally via the pngFallback directory setting
     if (!hasSvgSupport) {
-      var perElementFallback = el.getAttribute('data-fallback') || el.getAttribute('data-png');
+      const perElementFallback = el.getAttribute('data-fallback') || el.getAttribute('data-png');
 
       // Per-element specific PNG fallback defined, so use that
       if (perElementFallback) {
@@ -205,7 +204,7 @@
       }
       // Global PNG fallback directoriy defined, use the same-named PNG
       else if (pngFallback) {
-        el.setAttribute('src', pngFallback + '/' + imgUrl.split('/').pop().replace('.svg', '.png'));
+        el.setAttribute('src', `${pngFallback}/${imgUrl.split('/').pop().replace('.svg', '.png')}`);
         callback(null);
       }
       // um...
@@ -220,7 +219,7 @@
     // avoid a race condition if multiple injections for the same element are run.
     // :NOTE: Using indexOf() only _after_ we check for SVG support and bail,
     // so no need for IE8 indexOf() polyfill
-    if (injectedElements.indexOf(el) !== -1) {
+    if (injectedElements.includes(el)) {
       return;
     }
 
@@ -232,39 +231,36 @@
     el.setAttribute('src', '');
 
     // Load it up
-    loadSvg(imgUrl, function (svg) {
-
-      if (typeof svg === 'undefined' || typeof svg === 'string') {
+    loadSvg(imgUrl, svg => {
+      if (typeof svg == 'undefined' || typeof svg == 'string') {
         callback(svg);
         return false;
       }
 
-      var imgId = el.getAttribute('id');
+      const imgId = el.getAttribute('id');
       if (imgId) {
         svg.setAttribute('id', imgId);
       }
 
-      var imgTitle = el.getAttribute('title');
+      const imgTitle = el.getAttribute('title');
       if (imgTitle) {
         svg.setAttribute('title', imgTitle);
       }
 
       // Concat the SVG classes + 'injected-svg' + the img classes
-      var classMerge = [].concat(svg.getAttribute('class') || [], 'injected-svg', el.getAttribute('class') || []).join(' ');
+      const classMerge = [].concat(svg.getAttribute('class') || [], 'injected-svg', el.getAttribute('class') || []).join(' ');
       svg.setAttribute('class', uniqueClasses(classMerge));
 
-      var imgStyle = el.getAttribute('style');
+      const imgStyle = el.getAttribute('style');
       if (imgStyle) {
         svg.setAttribute('style', imgStyle);
       }
 
       // Copy all the data elements to the svg
-      var imgData = [].filter.call(el.attributes, function (at) {
-        return (/^data-\w[\w\-]*$/).test(at.name);
-      });
-      forEach.call(imgData, function (dataAttr) {
-        if (dataAttr.name && dataAttr.value) {
-          svg.setAttribute(dataAttr.name, dataAttr.value);
+      const imgData = [].filter.call(el.attributes, ({name}) => (/^data-\w[\w\-]*$/).test(name) || 'onclick'.match(name));
+      forEach.call(imgData, ({name, value}) => {
+        if (name && value) {
+          svg.setAttribute(name, value);
         }
       });
 
@@ -281,7 +277,7 @@
 
       // Handle all defs elements that have iri capable attributes as defined by w3c: http://www.w3.org/TR/SVG/linking.html#processingIRI
       // Mapping IRI addressable elements to the properties that can reference them:
-      var iriElementsAndProperties = {
+      const iriElementsAndProperties = {
         'clipPath': ['clip-path'],
         'color-profile': ['color-profile'],
         'cursor': ['cursor'],
@@ -293,23 +289,27 @@
         'radialGradient': ['fill', 'stroke']
       };
 
-      var element, elementDefs, properties, currentId, newId;
-      Object.keys(iriElementsAndProperties).forEach(function (key) {
+      let element;
+      let elementDefs;
+      let properties;
+      let currentId;
+      let newId;
+      Object.keys(iriElementsAndProperties).forEach(key => {
         element = key;
         properties = iriElementsAndProperties[key];
 
-        elementDefs = svg.querySelectorAll('defs ' + element + '[id]');
-        for (var i = 0, elementsLen = elementDefs.length; i < elementsLen; i++) {
+        elementDefs = svg.querySelectorAll(`defs ${element}[id]`);
+        for (let i = 0, elementsLen = elementDefs.length; i < elementsLen; i++) {
           currentId = elementDefs[i].id;
-          newId = currentId + '-' + injectCount;
+          newId = `${currentId}-${injectCount}`;
 
           // All of the properties that can reference this element type
-          var referencingElements;
-          forEach.call(properties, function (property) {
+          let referencingElements;
+          forEach.call(properties, property => {
             // :NOTE: using a substring match attr selector here to deal with IE "adding extra quotes in url() attrs"
-            referencingElements = svg.querySelectorAll('[' + property + '*="' + currentId + '"]');
-            for (var j = 0, referencingElementLen = referencingElements.length; j < referencingElementLen; j++) {
-              referencingElements[j].setAttribute(property, 'url(#' + newId + ')');
+            referencingElements = svg.querySelectorAll(`[${property}*="${currentId}"]`);
+            for (let j = 0, referencingElementLen = referencingElements.length; j < referencingElementLen; j++) {
+              referencingElements[j].setAttribute(property, `url(#${newId})`);
             }
           });
 
@@ -324,16 +324,17 @@
       // elements run, so we'll need to make that happen, if requested
 
       // Find then prune the scripts
-      var scripts = svg.querySelectorAll('script');
-      var scriptsToEval = [];
-      var script, scriptType;
+      const scripts = svg.querySelectorAll('script');
+      const scriptsToEval = [];
+      let script;
+      let scriptType;
 
-      for (var k = 0, scriptsLen = scripts.length; k < scriptsLen; k++) {
+      for (let k = 0, scriptsLen = scripts.length; k < scriptsLen; k++) {
         scriptType = scripts[k].getAttribute('type');
 
         // Only process javascript types.
         // SVG defaults to 'application/ecmascript' for unset types
-        if (!scriptType || scriptType === 'application/ecmascript' || scriptType === 'application/javascript') {
+        if (!scriptType || scriptType == 'application/ecmascript' || scriptType == 'application/javascript') {
 
           // innerText for IE, textContent for other browsers
           script = scripts[k].innerText || scripts[k].textContent;
@@ -347,8 +348,8 @@
       }
 
       // Run/Eval the scripts if needed
-      if (scriptsToEval.length > 0 && (evalScripts === 'always' || (evalScripts === 'once' && !ranScripts[imgUrl]))) {
-        for (var l = 0, scriptsToEvalLen = scriptsToEval.length; l < scriptsToEvalLen; l++) {
+      if (scriptsToEval.length > 0 && (evalScripts == 'always' || (evalScripts == 'once' && !ranScripts[imgUrl]))) {
+        for (let l = 0, scriptsToEvalLen = scriptsToEval.length; l < scriptsToEvalLen; l++) {
 
           // :NOTE: Yup, this is a form of eval, but it is being used to eval code
           // the caller has explictely asked to be loaded, and the code is in a caller
@@ -368,8 +369,8 @@
       // This trick will trigger IE to read and use any existing SVG <style> tags.
       //
       // Reference: https://github.com/iconic/SVGInjector/issues/23
-      var styleTags = svg.querySelectorAll('style');
-      forEach.call(styleTags, function (styleTag) {
+      const styleTags = svg.querySelectorAll('style');
+      forEach.call(styleTags, styleTag => {
         styleTag.textContent += '';
       });
 
@@ -403,37 +404,33 @@
    * @param {function} callback
    * @return {object} Instance of SVGInjector
    */
-  var SVGInjector = function (elements, options, done) {
-
-    // Options & defaults
-    options = options || {};
-
+  const SVGInjector = (elements, options = {}, done) => {
     // Should we run the scripts blocks found in the SVG
     // 'always' - Run them every time
     // 'once' - Only run scripts once for each SVG
     // [false|'never'] - Ignore scripts
-    var evalScripts = options.evalScripts || 'always';
+    const evalScripts = options.evalScripts || 'always';
 
     // Location of fallback pngs, if desired
-    var pngFallback = options.pngFallback || false;
+    const pngFallback = options.pngFallback || false;
 
     // Callback to run during each SVG injection, returning the SVG injected
-    var eachCallback = options.each;
+    const eachCallback = options.each;
 
     // Do the injection...
-    if (elements.length !== undefined) {
-      var elementsLoaded = 0;
-      forEach.call(elements, function (element) {
-        injectElement(element, evalScripts, pngFallback, function (svg) {
-          if (eachCallback && typeof eachCallback === 'function') eachCallback(svg);
-          if (done && elements.length === ++elementsLoaded) done(elementsLoaded);
+    if (elements && elements.length !== undefined) {
+      let elementsLoaded = 0;
+      forEach.call(elements, element => {
+        injectElement(element, evalScripts, pngFallback, svg => {
+          if (eachCallback && typeof eachCallback == 'function') eachCallback(svg);
+          if (done && elements.length == ++elementsLoaded) done(elementsLoaded);
         });
       });
     }
     else {
       if (elements) {
-        injectElement(elements, evalScripts, pngFallback, function (svg) {
-          if (eachCallback && typeof eachCallback === 'function') eachCallback(svg);
+        injectElement(elements, evalScripts, pngFallback, svg => {
+          if (eachCallback && typeof eachCallback == 'function') eachCallback(svg);
           if (done) done(1);
           elements = null;
         });
@@ -446,19 +443,16 @@
 
   /* global module, exports: true, define */
   // Node.js or CommonJS
-  if (typeof module === 'object' && typeof module.exports === 'object') {
+  if (typeof module == 'object' && typeof module.exports == 'object') {
     module.exports = exports = SVGInjector;
   }
   // AMD support
-  else if (typeof define === 'function' && define.amd) {
-    define(function () {
-      return SVGInjector;
-    });
+  else if (typeof define == 'function' && define.amd) {
+    define(() => SVGInjector);
   }
   // Otherwise, attach to window as global
-  else if (typeof window === 'object') {
+  else if (typeof window == 'object') {
     window.SVGInjector = SVGInjector;
   }
   /* global -module, -exports, -define */
-
-}(window, document));
+})(window, document);
